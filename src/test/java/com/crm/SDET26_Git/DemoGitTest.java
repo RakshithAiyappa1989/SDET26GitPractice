31701ddcbe00e8a0580d16d45b23a6b70ee0e7db
PC <PC@Surakshitha>
2021-12-09 13:46:59 +0530

    GitHub Successful

---
+++ src/test/java/com/crm/SDET26_Git/DemoGitTest.java
@@ -0,0 +1,11 @@
+package com.crm.SDET26_Git;
+
+import org.testng.annotations.Test;
+
+public class DemoGitTest {
+	
+	@Test
+	public void demoGitTest () {
+		System.out.println("Welcome to GitHub");
+	}
+}
